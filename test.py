356a58f51ef6e22417ae91506837465b34ed080d
BlueSky <42969183+FlyCuteBird@users.noreply.github.com>
2023-11-20 21:40:01 +0800

    Update test.py

--- test.py
+++ test.py
@@ -6,4 +6,7 @@ os.environ["CUDA_VISIBLE_DEVICES"] = '1'
 model_path = "./runs/runX/checkpoint/t2i/model_best.pth.tar"
 #model_path = "./runs/runX/checkpoint/i2t/model_best.pth.tar"
 data_path = "./data/"
+# for flickr30k
 evaluation.evalrank(model_path, data_path=data_path, split="test")
+# for coco
+#evaluation.evalrank(model_path, data_path=data_path, split="testall")
